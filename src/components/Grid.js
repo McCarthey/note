57fe49be7e2a26cdb429ef12aa37a05eb38eba7e
mccarthey <mccarthey123@outlook.com>
2019-05-14 23:03:23 +0800

    [format] css & update notes data

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GridLayout from 'react-grid-layout'
+import request from '../utils/request'
 import './grid.css'
 
 export default class MyFirstGrid extends React.Component {
@@ -12,12 +13,39 @@ export default class MyFirstGrid extends React.Component {
     }
 
     changeStop = layout => {
-        console.log(layout)
         this.setState({ layout })
     }
 
-    handleSave = () => {
-        localStorage.setItem('NOTE_DATA', JSON.stringify(this.state.layout))
+    handleSave = async () => {
+        const dataJSON = JSON.stringify(this.state.layout)
+        localStorage.setItem('NOTE_DATA', dataJSON)
+        try {
+            // TODO: 根据cookie-session查找请求者
+            await request.postJSON('/update', {
+            })
+            this.setState(
+                {
+                    snackbar: {
+                        open: true,
+                        message: '注册成功',
+                        type: 'success',
+                    },
+                },
+                () => {
+                    setTimeout(() => {
+                        this.props.history.push('/signin')
+                    }, 1000)
+                },
+            )
+        } catch (e) {
+            this.setState({
+                snackbar: {
+                    open: true,
+                    message: e.msg,
+                    type: 'error',
+                },
+            })
+        }
     }
 
     componentDidMount() {
@@ -43,15 +71,9 @@ export default class MyFirstGrid extends React.Component {
                     onDragStop={this.changeStop}
                     onResizeStop={this.changeStop}
                 >
-                    <div className="" key="a">
-                        a
-                    </div>
-                    <div className="" key="b">
-                        b
-                    </div>
-                    <div className="" key="c">
-                        c
-                    </div>
+                    <div key="a">a</div>
+                    <div key="b">b</div>
+                    <div key="c">c</div>
                 </GridLayout>
                 <button onClick={this.handleSave}>Save</button>
             </div>
