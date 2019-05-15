5206a1c5f2974ade5cb65ca62605b4453e33d31e
Randall <randall@duomai.com>
2019-05-15 17:23:05 +0800

    [add] dynimac add /remove items

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,16 +6,44 @@ import './grid.css'
 
 export default class MyFirstGrid extends React.Component {
     state = {
-        layout: [
-            { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-            { i: 'b', x: 1, y: 0, w: 3, h: 2 },
-            { i: 'c', x: 4, y: 0, w: 1, h: 2 },
-        ],
+        layout: [],
         snackbar: {
             open: false,
             message: '',
             type: '',
         },
+    }
+
+    createElement(el) {
+        const removeStyle = {
+            position: "absolute",
+            right: "2px",
+            top: 0,
+            cursor: "pointer",
+        };
+        const i = el.add ? "+" : el.i;
+        return (
+            <div key={i} data-grid={el}>
+                {el.add ? (
+                    <span
+                        className="add text"
+                        onClick={this.onAddItem}
+                        title="You can add an item by clicking here, too."
+                    >
+                        Add +
+              </span>
+                ) : (
+                        <span className="text">{i}</span>
+                    )}
+                <span
+                    className="remove"
+                    style={removeStyle}
+                    onClick={() => this.handleRemoveItem(i)}
+                >
+                    x
+            </span>
+            </div>
+        );
     }
 
     handleClose = () => {
@@ -28,8 +56,28 @@ export default class MyFirstGrid extends React.Component {
         })
     }
 
-    changeStop = layout => {
+    handleLayoutChange = layout => {
+        console.log(layout)
         this.setState({ layout })
+    }
+
+    handleRemoveItem(i) {
+        console.log(i)
+        this.setState({
+            layout: this.state.layout.filter(item => item.i !== i)
+        })
+    }
+
+    handleAddItem() {
+        this.setState({
+            layout: this.state.layout.concat({
+                i: (+ new Date()).toString(),
+                x: (this.state.layout.length * 2) % 12,
+                y: 0, // puts it at the bottom
+                w: 2,
+                h: 2
+            }),
+        });
     }
 
     async handleSave() {
@@ -90,14 +138,12 @@ export default class MyFirstGrid extends React.Component {
                     cols={12}
                     rowHeight={30}
                     width={1080}
-                    onDragStop={this.changeStop}
-                    onResizeStop={this.changeStop}
+                    onLayoutChange={this.handleLayoutChange}
                 >
-                    <div key="a">a</div>
-                    <div key="b">b</div>
-                    <div key="c">c</div>
+                    {this.state.layout.map(el => this.createElement(el))}
                 </GridLayout>
                 <button onClick={() => this.handleSave()}>Save</button>
+                <button onClick={() => this.handleAddItem()}> Add Item</button>
             </div>
         )
     }
