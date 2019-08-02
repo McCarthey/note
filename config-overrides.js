9701674f4dfd11cfcc471469a8de980d89a68c01
Randall <randall@duomai.com>
2019-08-02 15:40:38 +0800

    [fix] remove puppeteer config

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,1 @@
     
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const path = require('path');
-
-module.exports = (config, env) => {
-  if (env === 'production') {
-    config.plugins = config.plugins.concat([
-      new PrerenderSPAPlugin({
-        routes: ['/'],
-        staticDir: path.join(__dirname, 'build'),
-        outputDir: path.join(__dirname, 'prerender')
-      }),
-    ]);
-  }
-
-  return config;
-};
